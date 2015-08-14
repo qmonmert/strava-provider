595ed3253aed99ce868bf195110b525c903d9b53
Quentin Monmert <q.monmert@proxiad.com>
2015-08-14 16:17:15 +0200

    Initialize the project strava-provider

--- README.md
+++ README.md
@@ -1,38 +1,28 @@
 ### External data provider
 
-1.  API themoviedb
-
-    * https://www.themoviedb.org/documentation/api
-    * Example
-        * Search : http://api.themoviedb.org/3/search/movie?query=spiderman&api_key=xxx
-        * Find : http://api.themoviedb.org/3/find/tt0100669?api_key=xxx&external_source=imdb_id
-
-2. API strava
+1. API strava
 
     * http://strava.github.io/api/
     * Example
         * https://www.strava.com/api/v3/athlete?access_token=xxx
 
-3.  Components
+2.  Apply
 
-    * myMoviesList
-    * stravaAccount
+    * Code
+        * Complete the spring file : strava-provider.xml with your strava access_token
 
-4.  Apply
+    * Admin Jahia
+        * Create a site named : strava-site
+        * Deploy the module strava-provider on the site
 
-    * Complete the spring file : tmdb-provider.xml
-
-    * Deploy the module tmdb-provider
-
-    * themoviedb
-        * Create a new page in the ACMESPACE site
-        * Add a 'myMoviesList' component in this page
-        * Write spiderman in the title of the component
+    * Edit Mode Jahia
+        * Create a new page in the site
+        * Add a component named 'athlete' in this page
         * Look the result
 
-    * strava
-        * Create a new page in the ACMESPACE site
-        * Add a 'stravaAccount' component in this page
-        * Look the result
+3.  JCR
 
-
+    * Go to http://localhost:8080/modules/tools/jcrQuery.jsp
+    * Query :
+        * JCR-SQL2
+        * SELECT * FROM [jnt:stravaAccount]
--- src/main/java/org/jahia/modules/strava/StravaDataSource.java
+++ src/main/java/org/jahia/modules/strava/StravaDataSource.java
@@ -1,6 +1,9 @@
 package org.jahia.modules.strava;
 
 import com.google.common.collect.Sets;
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpsURL;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -8,6 +11,7 @@ import org.jahia.modules.external.ExternalData;
 import org.jahia.modules.external.ExternalDataSource;
 import org.jahia.modules.external.ExternalQuery;
 import org.jahia.modules.external.query.QueryHelper;
+import org.jahia.services.cache.ehcache.EhCacheProvider;
 import org.jahia.services.content.nodetypes.NodeTypeRegistry;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,29 +22,31 @@ import javax.jcr.RepositoryException;
 import java.util.*;
 
 /**
- * Created by Quentin on 18/07/15.
+ * Created by Quentin on 14/08/15.
  */
 public class StravaDataSource implements ExternalDataSource, ExternalDataSource.Searchable {
 
-    // ATTRIBUTES
-
+    // Strava API
     private static final String API_V3_ATHLETE = "/api/v3/athlete";
-
-    private static final String URL_STRAVA_ACCOUNT = "/stravaAccount/";
-
     private static final String ACCES_TOKEN = "access_token";
-
     private static final String URL_STRAVA = "www.strava.com";
-
     private static final Integer PORT_STRAVA = 443;
 
+    // Node types
     private static final String JNT_STRAVA_ACCOUNT = "jnt:stravaAccount";
+    private static final String JNT_CONTENT_FOLDER = "jnt:contentFolder";
 
     // Strava key account
     private String apiKeyValue;
 
     // Http client
     private HttpClient httpClient;
+
+    // Cache
+    private EhCacheProvider ehCacheProvider;
+    private Ehcache cache;
+    private static final String CACHE_NAME = "strava-cache";
+    private static final String CACHE_STRAVA_ACCOUNT  = "cacheStravaAccount";
 
     // CONSTRUCTOR
 
@@ -52,7 +58,19 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
 
     public void start() {
         // Init method defined in the bean : StravaDataSource
+        try {
+            if (!ehCacheProvider.getCacheManager().cacheExists(CACHE_NAME)) {
+                ehCacheProvider.getCacheManager().addCache(CACHE_NAME);
+            }
+            cache = ehCacheProvider.getCacheManager().getCache(CACHE_NAME);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (CacheException e) {
+            e.printStackTrace();
+        }
     }
+
+    // UTILS
 
     private JSONObject queryStrava(String path, String... params) throws RepositoryException {
         try {
@@ -61,10 +79,11 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
             Map<String, String> m = new LinkedHashMap<String, String>();
             m.put(ACCES_TOKEN, apiKeyValue);
             url.setQuery(m.keySet().toArray(new String[m.size()]), m.values().toArray(new String[m.size()]));
-            System.out.println("Start request : " + url);
+            System.out.println("=> Start request strava : " + url);
             GetMethod httpMethod = new GetMethod(url.toString());
             try {
                 httpClient.executeMethod(httpMethod);
+                cache.put(new Element(CACHE_STRAVA_ACCOUNT, new JSONObject(httpMethod.getResponseBodyAsString())));
                 return new JSONObject(httpMethod.getResponseBodyAsString());
             } finally {
                 httpMethod.releaseConnection();
@@ -75,7 +94,17 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
     }
 
     private String getPathForStravaAccount(JSONObject stravaAccount) throws JSONException {
-        return URL_STRAVA_ACCOUNT + stravaAccount.getString("id");
+        return "/" + stravaAccount.getString("id");
+    }
+
+    private JSONObject getCacheStravaAccount() throws RepositoryException {
+        JSONObject stravaAccount;
+        if (cache.get(CACHE_STRAVA_ACCOUNT) != null) {
+            stravaAccount = (JSONObject) cache.get(CACHE_STRAVA_ACCOUNT).getObjectValue();
+        } else {
+            stravaAccount = queryStrava(API_V3_ATHLETE);
+        }
+        return stravaAccount;
     }
 
     // OVERRIDE : ExternalDataSource
@@ -83,7 +112,10 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
     @Override
     public ExternalData getItemByIdentifier(String identifier) throws ItemNotFoundException {
         try {
-            JSONObject stravaAccount = queryStrava(API_V3_ATHLETE);
+            if (identifier.equals("root")) {
+                return new ExternalData(identifier, "/", "jnt:contentFolder", new HashMap<String, String[]>());
+            }
+            JSONObject stravaAccount = getCacheStravaAccount();
             Map<String, String[]> properties = new HashMap<String, String[]>();
             if (stravaAccount.getString("lastname") != null)
                 properties.put("lastname", new String[]{stravaAccount.getString("lastname")});
@@ -101,7 +133,7 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
                 properties.put("email", new String[]{stravaAccount.getString("email")});
             if (stravaAccount.getString("weight") != null)
                 properties.put("weight", new String[]{stravaAccount.getString("weight")});
-            ExternalData data = new ExternalData(identifier, getPathForStravaAccount(stravaAccount), JNT_STRAVA_ACCOUNT, properties);
+            ExternalData data = new ExternalData(identifier, "/" + identifier, JNT_STRAVA_ACCOUNT, properties);
             return data;
         } catch (Exception e) {
             throw new ItemNotFoundException(e);
@@ -110,42 +142,38 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
 
     @Override
     public ExternalData getItemByPath(String path) throws PathNotFoundException {
-//        String[] splitPath = path.split("/");
+        String[] splitPath = path.split("/");
         try {
-            return getItemByIdentifier("XXX"); // TODO:
+            if (splitPath.length <= 1) {
+                return getItemByIdentifier("root");
+            } else {
+                return getItemByIdentifier(splitPath[1]);
+            }
         } catch (ItemNotFoundException e) {
             throw new PathNotFoundException(e);
         }
-    }
-
-    // OVERRIDE : ExternalDataSource.Searchable
-
-    @Override
-    public List<String> search(ExternalQuery query) throws RepositoryException {
-        List<String> results = new ArrayList<String>();
-        String nodeType = QueryHelper.getNodeType(query.getSource());
-        try {
-            if (NodeTypeRegistry.getInstance().getNodeType(JNT_STRAVA_ACCOUNT).isNodeType(nodeType)) {
-                JSONObject jsonObject = queryStrava(API_V3_ATHLETE);
-                String path = getPathForStravaAccount(jsonObject);
-                results.add(path);
-            }
-        } catch (JSONException e) {
-            throw new RepositoryException(e);
-        }
-        return results;
     }
 
     // OVERRIDE : ExternalDataSource
 
     @Override
     public List<String> getChildren(String path) throws RepositoryException {
-        return Collections.emptyList();
+        List<String> r = new ArrayList<String>();
+        if (path.equals("/")) {
+            try {
+                JSONObject stravaAccount = getCacheStravaAccount();
+                String pathChild = stravaAccount.getString("id");
+                r.add(pathChild);
+            } catch (JSONException e) {
+                throw new RepositoryException(e);
+            }
+        }
+        return r;
     }
 
     @Override
     public Set<String> getSupportedNodeTypes() {
-        return Sets.newHashSet("jnt:contentFolder", JNT_STRAVA_ACCOUNT);
+        return Sets.newHashSet(JNT_CONTENT_FOLDER, JNT_STRAVA_ACCOUNT);
     }
 
     @Override
@@ -163,10 +191,32 @@ public class StravaDataSource implements ExternalDataSource, ExternalDataSource.
         return false;
     }
 
+    // OVERRIDE : ExternalDataSource.Searchable
+
+    @Override
+    public List<String> search(ExternalQuery query) throws RepositoryException {
+        List<String> results = new ArrayList<String>();
+        String nodeType = QueryHelper.getNodeType(query.getSource());
+        try {
+            if (NodeTypeRegistry.getInstance().getNodeType(JNT_STRAVA_ACCOUNT).isNodeType(nodeType)) {
+                JSONObject stravaAccount = getCacheStravaAccount();
+                String path = getPathForStravaAccount(stravaAccount);
+                results.add(path);
+            }
+        } catch (JSONException e) {
+            throw new RepositoryException(e);
+        }
+        return results;
+    }
+
     // GETTERS AND SETTERS
 
     public void setApiKeyValue(String apiKeyValue) {
         this.apiKeyValue = apiKeyValue;
+    }
+
+    public void setCacheProvider(EhCacheProvider ehCacheProvider) {
+        this.ehCacheProvider = ehCacheProvider;
     }
 
 }
